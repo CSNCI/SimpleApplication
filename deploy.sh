95cb139d1b8f0f49e908a753e83978883933f975
CristinaSiasA <x20214006@student.ncirl.ie>
2022-03-22 14:47:05 +0000

    added continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+#install pm2 which is a production process manager for Node.js with a bult-in loader balancer.
+sudo npm install -g pm2
+#stop any instance of our application running currently
+pm2 stop simple_app
+#change director into folder where application is downloaded
+cd SimpleApplication/
+#Install application dependencies
+npm install
+#Start the application with the process name simple_app using pm2
+pm2 start ./bin/222 --name simple_app
